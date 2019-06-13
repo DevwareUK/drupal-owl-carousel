b3ed8535cb5b9ec2f9d16641660c63d33537ebfe
Alex Burrows <alex@digidrop.io>
2019-06-13 11:47:39 +0100

    Added custom JS usage for owlcarousel and basic settings

--- js/owlcarousel.js
+++ js/owlcarousel.js
@@ -1,27 +1,36 @@
 /**
  * @file
+ * OwlCarousel Drupal JS.
  */
 
 (function ($, Drupal, drupalSettings) {
     'use strict';
 
+    // @todo: Currently this is breaking JS in Drupal.
     Drupal.behaviors.owl = {
         attach: function (context, settings) {
-            $('.owl-slider-wrapper', context).each(function () {
-                var $this = $(this);
-                var $this_settings = $.parseJSON($this.attr('data-settings'));
-                $this.owlCarousel($this_settings);
-            });
+            // $('.owl-slider-wrapper', context).each(function () {
+            //     var $this = $(this);
+            //     var $this_settings = $.parseJSON($this.attr('data-settings'));
+            //     $this.owlCarousel($this_settings);
+            // });
         }
     };
 
-    // @todo: set owlcarousel to work with multiple carousels.
+    /**
+     * OwlCarousel views js.
+     * @type {{attach: Drupal.behaviors.owlcarousel_views.attach}}
+     */
     Drupal.behaviors.owlcarousel_views = {
         attach: function (context, settings) {
+
+            // Declare the owlcarousel views settings object.
             var owlCarouselViews = drupalSettings.owlcarousel_views;
-            $('.owl-slider-wrapper').owlCarousel({
-                items: 1
-            });
+            // Loop the carousel object and output settings into our carousel.
+            for (const item in owlCarouselViews) {
+                var thisSettings = owlCarouselViews[item];
+                $('.' + item).owlCarousel(thisSettings);
+            }
 
         }
     }
--- src/Plugin/views/style/OwlCarouselViews.php
+++ src/Plugin/views/style/OwlCarouselViews.php
@@ -56,14 +56,45 @@ class OwlCarouselViews extends StylePluginBase {
 
     $fields = ['' => t('<None>')];
     $fields += $this->displayHandler->getFieldLabels(TRUE);
-
-    $form['items'] = [
+    $form['default_options'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Use default settings'),
+      '#description' => $this->t('By selecting this the default settings will be used and overwrite your custom settings.'),
+      '#default_value' => $this->options['default_options'],
+    ];
+    $form['custom_settings'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Custom settings'),
+    ];
+    $form['custom_settings']['items'] = [
       '#type' => 'number',
       '#title' => $this->t('Items'),
       '#description' => $this->t('Maximum amount of items displayed at a time with the widest browser width.'),
-      '#default_value' => $this->options['items'],
+      '#default_value' => $this->options['custom_settings']['items'],
     ];
-
+    $form['custom_settings']['margin'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Margin (px)'),
+      '#default_value' => $this->options['custom_settings']['margin'],
+    ];
+    $form['custom_settings']['loop'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Loop'),
+      '#default_value' => $this->options['custom_settings']['loop'],
+      '#description' => $this->t('Infinity loop. Duplicate last and first items to get loop illusion.'),
+    ];
+    $form['custom_settings']['center'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Center'),
+      '#description' => $this->t('Center item. Works well with even an odd number of items.'),
+      '#default_value' => $this->options['custom_settings']['center'],
+    ];
+    $form['custom_settings']['mouseDrag'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('mouseDrag'),
+      '#description' => $this->t('Mouse drag enabled.'),
+      '#default_value' => $this->options['custom_settings']['mouseDrag'],
+    ];
 
   }
 
