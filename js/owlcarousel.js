444e20263139795f066592f92293adf4d9946130
James Rogers <james.rogers@devware.co.uk>
2021-04-16 12:56:03 +0100

    Fix issue with clicking controls changing the slide

--- js/owlcarousel.js
+++ js/owlcarousel.js
@@ -3,29 +3,22 @@
  */
 
 (function ($) {
-    Drupal.behaviors.owl = {
-        attach: function (context, settings) {
-            let carousels = [];
+  Drupal.behaviors.owl = {
+    attach: function (context, settings) {
+      let carousels = [];
 
-            $('.owl-slider-wrapper', context).each(function () {
-                var $this = $(this);
-                var $this_settings = $.parseJSON($this.attr('data-settings'));
-                carousels.push($this.owlCarousel($this_settings));
+      $('.owl-slider-wrapper', context).each(function () {
+        var $this = $(this);
+        var $this_settings = $.parseJSON($this.attr('data-settings'));
+        carousels.push($this.owlCarousel($this_settings));
 
-                if ($(this).parent().hasClass('owlcarousel-thumbs-thumbs')) {
-                  console.log(carousels);
-                  console.log(carousels.length);
-                  console.log(carousels[carousels.length -2]);
-
-                  $this.click({main: carousels[carousels.length -2]}, (e) => {
-                    console.log(e.data.main);
-                    var index = $(e.target).closest('.owl-item').index();
-                    e.data.main.trigger('owl.goTo', [index, 300]);
-                  }).bind(carousels.length);
-                }
-
-            });
-
+        if ($(this).parent().hasClass('owlcarousel-thumbs-thumbs')) {
+          $this.find('.owl-wrapper').click({main: carousels[carousels.length - 2]}, (e) => {
+            var index = $(e.target).closest('.owl-item').index();
+            e.data.main.trigger('owl.goTo', [index, 300]);
+          }).bind(carousels.length);
         }
-    };
+      });
+    }
+  };
 })(jQuery);
